4359cef77a352920a8032c422754cb60d57bb6b2
hilalex07 <alexandre.hilaire07@gmail.com>
2024-03-03 11:22:03 +0100

    PhosphorIcons imported and functional, auto import added to

--- CraftedBy/src/components/TheWelcome.vue
+++ CraftedBy/src/components/TheWelcome.vue
@@ -1,13 +1,15 @@
 <script setup>
-
+import { PhHorse, PhHeart, PhCube } from "@phosphor-icons/vue";
 </script>
 
 <template>
 
 <h1 class="text-6xl">Bienvenue</h1>
 <button class="btn btn-error">button</button>
-<button
-  class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+<button class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
   Button
 </button>
+<PhHorse />
+<PhHeart :size="32" color="hotpink" weight="fill"/>
+<PhCube/>
 </template>
--- CraftedBy/src/main.js
+++ CraftedBy/src/main.js
@@ -1,7 +1,10 @@
 import './assets/main.css';
+
 import axios from 'axios';
+
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+
 
 import App from './App.vue';
 import router from './router';
