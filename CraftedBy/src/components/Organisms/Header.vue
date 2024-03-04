396797b1cb64a91e7952fb84f170c574941e6b68
Alexandre-Hilaire <alexandre.hilaire@le-campus-numerique.fr>
2024-03-04 15:07:58 +0100

    logo is in public folder now, some responsive design

--- CraftedBy/src/components/Organisms/Header.vue
+++ CraftedBy/src/components/Organisms/Header.vue
@@ -1,12 +1,10 @@
 <template>
-    <div class="flex justify-center ">
-
-        <CraftedBy class="flex align-self-center" />
-    <div>
-        <DarkLightModeButton />
+    <div class="flex justify-center">
+        <CraftedBy class="flex align-self-center w-64 md:w-auto" />
+        <div>
+            <DarkLightModeButton />
+        </div>
     </div>
-    </div>
 
-
-    <NavBar class="sticky top-0" />
+    <NavBar class="sticky top-0 w-auto" />
 </template>
