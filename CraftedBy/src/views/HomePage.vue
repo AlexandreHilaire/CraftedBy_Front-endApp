6882034832acb44ddb5b42ca6cc182fa198a3d3b
Alexandre-Hilaire <alexandre.hilaire@le-campus-numerique.fr>
2024-03-04 09:57:02 +0100

    add buttons components, logo png

---
+++ CraftedBy/src/views/HomePage.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+import TheWelcome from '../components/TheWelcome.vue'
+import ButtonPrimary from '../components/Buttons/ButtonPrimary.vue';
+import ButtonWarning from '@/components/Buttons/ButtonWarning.vue';
+import ButtonDefault from '@/components/Buttons/ButtonDefault.vue';
+
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    <ButtonPrimary />
+    <ButtonWarning />
+    <ButtonDefault/>
+  </main>
+</template>
