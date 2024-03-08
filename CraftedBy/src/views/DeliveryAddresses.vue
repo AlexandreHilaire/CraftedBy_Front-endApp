95ac67b50c8138ed0c71a789dd75057c001a66b0
Alexandre-Hilaire <alexandre.hilaire@le-campus-numerique.fr>
2024-03-08 16:43:45 +0100

    axios data

--- CraftedBy/src/views/DeliveryAddresses.vue
+++ CraftedBy/src/views/DeliveryAddresses.vue
@@ -1,6 +1,23 @@
+<script setup>
+import axios from 'axios';
+import { ref } from 'vue';
+
+const user = ref();
+axios.get('https://fakestoreapi.com/users/1')
+  .then(response => {
+    console.log(response.data);
+    user.value = response.data;
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+</script>
+
 <template>
 
-<Steps />
-<h1> test </h1>
-
+<div class="flex mx-auto place-content-center">
+    <Steps />
+</div>
+    <p>{{ user.username }}</p>
 </template>
