10fbdf073179c3a5b10c503210616b6f6365d5b1
hilalex07 <hilalex07@PC-Alex>
2024-03-16 10:40:49 +0100

    edit DeliveryAddresses view

--- CraftedBy/src/views/DeliveryAddresses.vue
+++ CraftedBy/src/views/DeliveryAddresses.vue
@@ -15,40 +15,40 @@ axios
 </script>
 
 <template>
-  <div class="mx-auto flex place-content-center">
+<div class="mx-auto flex flex-col items-center">
     <Steps />
-  </div>
-  <p>{{ user.username }}</p>
-  <div class="flex justify-center space-x-80">
-    <div class="card w-96 bg-slate-600 text-neutral-content">
-      <div class="card-body items-center text-center">
-        <h2 class="card-title">Selectionnez une adresse de livraison</h2>
-        <div class="card-actions justify-end">
-          <select class="select select-bordered w-full max-w-xs">
-            <option v-for="address in user.address" :key="address" value="address">
-              {{ address }}
-            </option>
-          </select>
+    <p>{{ user.username }}</p>
+    <div class="flex flex-row w-full space-x-4 justify-center m-10">
+      <div class="card w-96 bg-slate-600 text-neutral-content">
+        <div class="card-body items-center text-center">
+          <h2 class="card-title">Selectionnez une adresse de livraison</h2>
+          <div class="card-actions justify-end">
+            <select class="select select-bordered w-full max-w-xs">
+              <option v-for="address in user.address" :key="address" value="address">
+                {{ address }}
+              </option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <div class="divider divider-horizontal"></div>
+      <div class="card w-96 bg-slate-600 text-neutral-content">
+        <div class="card-body items-center text-center">
+          <h2 class="card-title">Selectionnez une adresse de facturation</h2>
+          <div class="card-actions justify-end">
+            <select class="select select-bordered w-full max-w-xs">
+              <option v-for="address in user.address" :key="address" value="address">
+                {{ address }}
+              </option>
+            </select>
+          </div>
         </div>
       </div>
     </div>
-
-    <div class="card w-96 bg-slate-600 text-neutral-content">
-      <div class="card-body items-center text-center">
-        <h2 class="card-title">Selectionnez une adresse de facturation</h2>
-        <div class="card-actions justify-end">
-          <select class="select select-bordered w-full max-w-xs">
-            <option v-for="address in user.address" :key="address" value="address">
-              {{ address }}
-            </option>
-          </select>
-        </div>
-      </div>
+    <div class="flex place-content-center mt-4">
+      <RouterLink :to="{ name: 'deliveryMethod' }">
+        <ButtonPrimary :label="'Poursuivre'" />
+      </RouterLink>
     </div>
-  </div>
-  <div class="flex place-content-center">
-    <RouterLink :to="{ name: 'deliveryMethod' }">
-      <ButtonPrimary :label="'Poursuivre'" />
-    </RouterLink>
   </div>
 </template>
