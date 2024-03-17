10c0d3e74b1e42d82be2868b8be8ee45f7004960
hilalex07 <hilalex07@PC-Alex>
2024-03-17 15:37:00 +0100

    create crafter list card

--- CraftedBy/src/views/CraftersListView.vue
+++ CraftedBy/src/views/CraftersListView.vue
@@ -1,4 +1,13 @@
+<script setup>
+import CraftersListCard from '@/components/Organisms/CraftersListCard.vue';
+
+let crafters = ['crafter1', 'carter2', 'crafter3', 'crafter4'];
+
+</script>
+
 <template>
     <h1 class="text-2xl flex justify-center">Voici nos artisans</h1>
-    
+    <div v-for="crafter in crafters" :key="crafter" class="flex flex-col items-center m-20">
+    <CraftersListCard />
+    </div>
 </template>
