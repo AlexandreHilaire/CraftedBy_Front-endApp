ed68cf19ec1ca27552da5079e07eab204ef04df1
hilalex07 <hilalex07@PC-Alex>
2024-03-20 19:49:50 +0100

    adding welcome message

--- CraftedBy/src/components/TheWelcome.vue
+++
@@ -1,8 +0,0 @@
-<script setup>
-import { PhHorse, PhHeart, PhCube } from "@phosphor-icons/vue";
-</script>
-
-<template>
-
-<h1 class="text-6xl">Bienvenue</h1>
-</template>
--- CraftedBy/src/views/HomePage.vue
+++ CraftedBy/src/views/HomePage.vue
@@ -3,7 +3,14 @@
 </script>
 
 <template>
-  <main>
-    <p>toto</p>
-  </main>
+<div class="flex flex-col justify-center items-center m-5">
+  <h1 class ="text-4xl m-5">Bienvenue</h1>
+  <p class="max-w-3xl"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vestibulum nulla ut luctus
+          placerat. Ut vel mattis purus. Sed porta odio ut dictum feugiat. Phasellus leo risus,
+          tristique nec varius non, iaculis ut ante. Sed ornare non massa at volutpat. Nunc vel
+          convallis urna.</p>
+</div>
+
+
+  
 </template>
