1a988fd02296657c0edb7b6ff40935deb9a9d256
hilalex07 <hilalex07@PC-Alex>
2024-03-20 20:34:36 +0100

    structuring page

--- CraftedBy/src/views/HomePage.vue
+++ CraftedBy/src/views/HomePage.vue
@@ -3,14 +3,35 @@
 </script>
 
 <template>
-<div class="flex flex-col justify-center items-center m-5">
-  <h1 class ="text-4xl m-5">Bienvenue</h1>
-  <p class="max-w-3xl"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vestibulum nulla ut luctus
-          placerat. Ut vel mattis purus. Sed porta odio ut dictum feugiat. Phasellus leo risus,
-          tristique nec varius non, iaculis ut ante. Sed ornare non massa at volutpat. Nunc vel
-          convallis urna.</p>
-</div>
+  <div class="m-5 flex flex-col items-center justify-center">
+    <h1 class="m-5 text-8xl">Bienvenue</h1>
+    <p class="max-w-3xl">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vestibulum nulla ut luctus
+      placerat. Ut vel mattis purus. Sed porta odio ut dictum feugiat. Phasellus leo risus,
+      tristique nec varius non, iaculis ut ante. Sed ornare non massa at volutpat. Nunc vel
+      convallis urna.
+    </p>
+  </div>
 
+  <div class="m-5 flex flex-col items-center justify-center">
+    <h2 class="relative right-64 text-7xl">Le geste</h2>
+    <div class="bg-[#E0B841] bg-opacity-40 min-h-96 min-w-full flex justify-center">
+      <h2 class="relative left-16 text-7xl">des artisans</h2>
+    </div>
+  </div>
 
-  
+  <div class="m-5 flex flex-col items-center justify-center">
+    <h2 class="relative right-64 text-7xl">Produits</h2>
+    <div class="bg-blue-700 bg-opacity-40 min-h-96 min-w-full flex justify-center">
+      <h2 class="relative text-7xl">Phares</h2>
+    </div>
+  </div>
+
+  <div class="m-5 flex flex-col items-center justify-center">
+    <h2 class="relative right-64 text-7xl">Artisans</h2>
+    <div class="bg-red-700 bg-opacity-40 min-h-96 min-w-full flex-col justify-center items-center text-center">
+      <h2 class="relative text-7xl">Du</h2>
+      <h2 class="relative left-52 text-7xl">Mois</h2>
+    </div>
+  </div>
 </template>
