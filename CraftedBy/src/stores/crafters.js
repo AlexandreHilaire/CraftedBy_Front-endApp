e22b175b287f547243e02936410e5df45822f263
Alexandre-Hilaire <alexandre.hilaire@le-campus-numerique.fr>
2024-03-29 12:07:08 +0100

    fetch crafters in crafters list view

---
+++ CraftedBy/src/stores/crafters.js
@@ -0,0 +1,22 @@
+import { defineStore } from 'pinia';
+import axios from 'axios';
+import { ref } from 'vue';
+
+export const useCraftersStore = defineStore('crafter', () => {
+
+    const apiUrl = import.meta.env.VITE_API_URL;
+    const crafters = ref([]);
+
+    async function fetchCrafters () {
+        try {
+            const response = await axios.get(`${apiUrl}/crafters`);
+            crafters.value = response.data;
+        }
+        catch (error) {
+            console.log("error fetching data", error);
+        }
+    }
+
+    return {fetchCrafters, crafters};
+
+});
