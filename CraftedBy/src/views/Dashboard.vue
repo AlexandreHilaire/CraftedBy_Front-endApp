13207ae22acf5dcaad92da1459bab6b42deab6b2
Alexandre-Hilaire <alexandre.hilaire@le-campus-numerique.fr>
2024-04-02 11:50:36 +0200

    dashboard

--- CraftedBy/src/views/Dashboard.vue
+++ CraftedBy/src/views/Dashboard.vue
@@ -14,7 +14,15 @@ onBeforeMount(async () => {
 
 <template>
     <h1>Profil</h1>
-    <div class="flex justify-center">
-        
+    <div class="flex justify-center items-center flex-col">
+        <div class="avatar">
+            <div class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            </div>
+        </div>
+        <div>
+            <h2 class="m-5 text-2xl">Vos informations</h2>
+            
+        </div>
     </div>
 </template>
