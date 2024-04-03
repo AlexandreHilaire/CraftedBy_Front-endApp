10a389c0a091f58d96efb3909530866358a52a72
Alexandre-Hilaire <alexandre.hilaire@le-campus-numerique.fr>
2024-04-03 16:44:39 +0200

    adding cartItems for order creation

--- CraftedBy/src/stores/orders.js
+++ CraftedBy/src/stores/orders.js
@@ -1,15 +1,18 @@
 import { defineStore } from 'pinia';
-import axios from 'axios';
 import { ref } from 'vue';
 
 export const useOrderStore = defineStore('order', () => {
 
     const selectedDeliveryAddress = ref();
     const selectedBillingAddress = ref();
+    const cartItems = ref();
 
+    const validatedCart = (cartItemsValidated) => {
+        cartItems.value = cartItemsValidated;
+    }
     const validatedAddresses = (selectedDeliveryAddressByUser, selectedBillingAddressByUser) => {
         selectedDeliveryAddress.value = selectedDeliveryAddressByUser;
         selectedBillingAddress.value = selectedBillingAddressByUser;
     }
-    return {validatedAddresses, selectedDeliveryAddress, selectedBillingAddress};
+    return {validatedAddresses, validatedCart, selectedDeliveryAddress, selectedBillingAddress, cartItems};
 });
