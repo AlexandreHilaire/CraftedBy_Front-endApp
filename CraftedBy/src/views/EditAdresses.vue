42bd80fe568fc5cd82c36e5bbf5fb4adb305db40
Alexandre-Hilaire <alexandre.hilaire@le-campus-numerique.fr>
2024-05-27 14:45:54 +0200

    create editAdresses view

---
+++ CraftedBy/src/views/EditAdresses.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+<template>
+  <div class="flex flex-col justify-center items-center gap-5">
+    <h1>Modifier mes adresses</h1>
+    <p>Nom adresse</p>
+    <p>first adresse</p>
+    <p>postal code</p>
+  </div>
+</template>
