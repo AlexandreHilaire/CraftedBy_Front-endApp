ae462acea7b34a0454a57fef984fae1fd401fcd2
Alexandre-Hilaire <alexandre.hilaire@le-campus-numerique.fr>
2024-05-28 14:34:08 +0200

    adding a condition wich watch route and show the correct form

--- CraftedBy/src/components/Molecules/GenericForms.vue
+++ CraftedBy/src/components/Molecules/GenericForms.vue
@@ -15,6 +15,7 @@ onBeforeMount(async () => {
 
 const route = useRoute();
 
+// crafter form
 const crafter_name = ref('');
 const location = ref('');
 const information = ref('');
@@ -27,6 +28,9 @@ const submitCreateCrafter = async () => {
     await crafterStore.createCrafter(user_id,crafter_name.value, location.value, information.value, story.value, crafting_process.value, material_preference.value);
 }
 
+// Adress form
+const searchAdress= ref('');
+
 const submitForm = async () => {
     if (route.name === "createCrafter"){
         await submitCreateCrafter();
@@ -37,19 +41,24 @@ const submitForm = async () => {
 }
 </script>
 <template>
-        <form @submit.prevent='submitForm' class="flex flex-col">
-            <label class="input input-bordered flex items-center gap-2 m-2">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg>
-            <input type="text" class="grow" placeholder="Nom d'artisant" v-model="crafter_name"/>
-            </label>
-            <label class="input input-bordered flex items-center gap-2 m-2">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg>
-            <input type="text" class="grow" placeholder="Emplacement" v-model="location"/>
-            </label>
-            <textarea class="textarea textarea-bordered m-2" placeholder="Présentation" v-model="information"></textarea>
-            <textarea class="textarea textarea-bordered m-2" placeholder="Histoire" v-model="story"></textarea>
-            <textarea class="textarea textarea-bordered m-2" placeholder="mon savoir faire" v-model="crafting_process"></textarea>
-            <textarea class="textarea textarea-bordered m-2" placeholder="Ce que j'aime le plus travailler" v-model="material_preference"></textarea>
+        <form @submit.prevent='submitForm' class="flex flex-col gap-2">
+            <div v-if="route.name === 'createCrafter'" class="flex flex-col">
+                <label class="input input-bordered flex items-center gap-2 m-2">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg>
+                <input type="text" class="grow" placeholder="Nom d'artisant" v-model="crafter_name"/>
+                </label>
+                <label class="input input-bordered flex items-center gap-2 m-2">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg>
+                <input type="text" class="grow" placeholder="Emplacement" v-model="location"/>
+                </label>
+                <textarea class="textarea textarea-bordered m-2" placeholder="Présentation" v-model="information"></textarea>
+                <textarea class="textarea textarea-bordered m-2" placeholder="Histoire" v-model="story"></textarea>
+                <textarea class="textarea textarea-bordered m-2" placeholder="mon savoir faire" v-model="crafting_process"></textarea>
+                <textarea class="textarea textarea-bordered m-2" placeholder="Ce que j'aime le plus travailler" v-model="material_preference"></textarea>
+            </div>
+            <div v-if="route.name === 'createAdress'">
+                <textarea class="textarea textarea-bordered" placeholder="recherchez votre adresse" v-model="searchAdress"></textarea>
+            </div>
             <ValidateFormsButton label="Valider" />
         </form>
 </template>
