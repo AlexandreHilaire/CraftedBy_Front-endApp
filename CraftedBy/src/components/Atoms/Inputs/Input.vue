dd2c6e7060a63df785725fadd08cc1d070881331
Alexandre-Hilaire <alexandre.hilaire@le-campus-numerique.fr>
2024-05-29 12:20:03 +0200

    remove generic input for the moment

--- CraftedBy/src/components/Atoms/Inputs/Input.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-  const props = defineProps({
-    type: String,
-    placeholder: String,
-  });
-</script>
-<template>
-  <input :type="props.type" :placeholder="props.placeholder" class="input input-bordered input-primary w-full max-w-xs" />
-</template>
