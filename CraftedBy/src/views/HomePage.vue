fdc0872c5a6f57d624343d88e96a744a48d61356
hilalex07 <hilalex07@hotmail.fr>
2024-07-10 20:32:51 +0200

    ajustement du responsif des titres

--- CraftedBy/src/views/HomePage.vue
+++ CraftedBy/src/views/HomePage.vue
@@ -4,7 +4,7 @@ let contents = ['1', '2']
 
 <template>
   <div class="m-5 flex flex-col items-center justify-center">
-    <h1 class="m-5 text-8xl">Bienvenue</h1>
+    <h1 class="m-5 lg:text-8xl md:text-6xl sm:text-5xl">Bienvenue</h1>
     <p class="max-w-3xl">
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vestibulum nulla ut luctus
       placerat. Ut vel mattis purus. Sed porta odio ut dictum feugiat. Phasellus leo risus,
@@ -14,9 +14,9 @@ let contents = ['1', '2']
   </div>
 
   <div class="m-5 flex flex-col items-center justify-center">
-    <h2 class="relative right-24 text-7xl">Le geste</h2>
+    <h2 class="relative right-24 lg:text-7xl md:text-5xl sm:text-3xl">Le geste</h2>
     <div class="relative flex min-h-96 min-w-full justify-center bg-[#E0B841] bg-opacity-40">
-      <h2 class="absolute left-2/4 text-7xl">des artisans</h2>
+      <h2 class="absolute left-2/4 lg:text-7xl md:text-5xl sm:text-3xl">des artisans</h2>
       <div
         v-for="content in contents"
         :key="content"
@@ -38,9 +38,9 @@ let contents = ['1', '2']
   </div>
 
   <div class="m-5 flex flex-col items-center justify-center">
-    <h2 class="relative right-24 text-7xl">Produits</h2>
+    <h2 class="relative right-24 lg:text-7xl md:text-5xl sm:text-3xl">Produits</h2>
     <div class="relative flex min-h-96 min-w-full justify-center bg-blue-700 bg-opacity-40">
-      <h2 class="absolute left-2/4 text-7xl">Phares</h2>
+      <h2 class="absolute left-2/4 lg:text-7xl md:text-5xl sm:text-3xl">Phares</h2>
       <div
         v-for="content in contents"
         :key="content"
@@ -62,11 +62,11 @@ let contents = ['1', '2']
   </div>
 
   <div class="m-5 flex flex-col items-center justify-center">
-    <h2 class="relative right-52 text-7xl">Artisans</h2>
+    <h2 class="relative right-52 lg:text-7xl md:text-5xl sm:text-3xl">Artisans</h2>
     <div
       class="relative flex min-h-96 min-w-full justify-center bg-red-700 bg-opacity-40 text-center">
-      <h2 class="absolute right-1/2 text-7xl">Du</h2>
-      <h2 class="absolute top-16 start-1/2 text-7xl">Mois</h2>
+      <h2 class="absolute right-1/2 lg:text-7xl md:text-5xl sm:text-3xl">Du</h2>
+      <h2 class="absolute top-16 start-1/2 lg:text-7xl md:text-5xl sm:text-3xl">Mois</h2>
       <div
         v-for="content in contents"
         :key="content"
