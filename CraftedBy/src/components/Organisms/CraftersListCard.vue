39234f21d824a28c8c12af63cb8b54e9717c3942
hilalex07 <hilalex07@hotmail.fr>
2024-07-11 20:20:38 +0200

    responsive crafter list card

--- CraftedBy/src/components/Organisms/CraftersListCard.vue
+++ CraftedBy/src/components/Organisms/CraftersListCard.vue
@@ -8,12 +8,12 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="card card-side bg-base-100 shadow-xl w-2/4">
+  <div class="card card-side bg-base-100 shadow-xl flex place-items-center flex-wrap md:flex-nowrap">
     <figure>
       <img src="/crafter.svg" alt="crafter" class="w-full object-contain" />
     </figure>
     <div class="card-body">
-      <h2 class="card-title">{{props.crafter.crafter_name}}</h2>
+      <h2 class="card-title md:text-5xl sm:text-2xl">{{props.crafter.crafter_name}}</h2>
       <p>{{props.crafter.information}}</p>
       <div class="card-actions justify-end">
         <RouterLink :to="{ name: 'crafter', params: { id: props.crafter.id } }">
