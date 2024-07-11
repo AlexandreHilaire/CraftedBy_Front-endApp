86e2f051aea62b0d95ee34f5707c4ca141cbc7c4
hilalex07 <hilalex07@hotmail.fr>
2024-07-11 20:18:39 +0200

    Responsive product card and adding v-if for editing product button

--- CraftedBy/src/components/Organisms/ProductCard.vue
+++ CraftedBy/src/components/Organisms/ProductCard.vue
@@ -1,14 +1,17 @@
 <script setup>
 import { userCartStore } from '@/stores/cart';
 import { useProductStore } from '@/stores/product';
+import { useUserStore } from '@/stores/user';
 import {  storeToRefs } from 'pinia';
-import { ref } from 'vue';
+import { onBeforeMount, ref } from 'vue';
 import { useRoute } from 'vue-router';
 
+const userStore = useUserStore();
 const route = useRoute();
 const product_id = ref(route.params.id);
 const cartStore = userCartStore();
 const productStore = useProductStore();
+const user = ref();
 
 productStore.getProductData(product_id.value);
 
@@ -18,23 +21,31 @@ function addProductToCart() {
   cartStore.addProductToCart(productData.value);
 }
 
+onBeforeMount(async () => {
+  user.value = await userStore.userAuth()
+  const userId = user.value.id
+  await userStore.fetchUserRole(userId)
+});
+
 </script>
 
 <template>
-  <div v-if="productData" class="card card-side bg-base-100 shadow-xl flex place-items-center w-1/2 mx-auto flex-wrap md:flex-nowrap">
+  <div v-if="productData" class="card card-side bg-base-100 shadow-xl flex place-items-center flex-wrap md:flex-nowrap">
     <figure>
       <img :src="productData.images[0]" class="w-full object-contain" />
     </figure>
     <div class="card-body">
-       <h2 class="card-title">{{ productData.name }}</h2>
+      <h2 class="card-title">{{ productData.name }}</h2>
       <p>{{ productData.description }}</p>
       <p class="font-semibold justify-end">{{ productData.unit_price }} €</p>
       <div class="card-actions justify-end">
         <ButtonPrimary @click="addProductToCart" :label="'Acheter'" />
-        <RouterLink :to="{name:'editProduct', params: {ProductId: product_id}}">
-            <ButtonPrimary label="Editer le produit"/>
-        </RouterLink>
-       </div>
+        <div v-if="userStore.userRole === 'crafter'">
+          <RouterLink :to="{name:'editProduct', params: {ProductId: product_id}}">
+              <ButtonPrimary label="Editer le produit"/>
+          </RouterLink>
+        </div>
+      </div>
     </div>
   </div>
 </template>
